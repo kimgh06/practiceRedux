c400244a2aafceba35539978ea5c66e0130f5458
kkh061101 <kkh061101@naver.com>
2023-05-09 17:54:21 +0900

    plus & minus button by using redux

--- src/index.js
+++ src/index.js
@@ -12,20 +12,33 @@ const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const value = document.getElementById("value");
 
+const add = "add";
+const minuss = "minus";
 const reducer = (cnt = 0, action) => {
   switch (action.type) {
-    case "add":
+    case add:
       cnt++;
       break;
-    case "minus":
+    case minuss:
       cnt--;
       break;
     default:
   }
+  console.log(cnt);
   return cnt;
 };
 
 const store = createStore(reducer);
-store.dispatch({ type: "add" });
 
-console.log(store.getState());
+const onchange = e => {
+  value.innerText = store.getState();
+}
+store.subscribe(onchange);
+
+plus.addEventListener("click", e => {
+  store.dispatch({ type: add });
+});
+
+minus.addEventListener("click", e => {
+  store.dispatch({ type: minuss });
+});
