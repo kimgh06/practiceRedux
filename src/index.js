45191bcc2659bb6c0a9e1781a6dfc737d2b08b6f
kkh061101 <kkh061101@naver.com>
2023-05-09 22:48:43 +0900

    react-redux start

--- src/index.js
+++ src/index.js
@@ -1,44 +1,90 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <App />
-// );
-import { createStore } from "redux";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
+// import { createStore } from "redux";
+// const form = document.querySelector("form");
+// const input = document.querySelector("input");
+// const ul = document.querySelector("ul");
 
-const plus = document.getElementById("plus");
-const minus = document.getElementById("minus");
-const value = document.getElementById("value");
+// const reducer = (arr = [], action) => {
+//   switch (action.type) {
+//     case "add":
+//       const a = { text: action.text, id: Date.now() }
+//       return [a, ...arr];
+//     case "del":
+//       return arr.filter(e => e.id !== action.id);
+//     default:
+//       return arr;
+//   }
+// }
+// const store = createStore(reducer);
 
-const add = "add";
-const minuss = "minus";
-const reducer = (cnt = 0, action) => {
-  switch (action.type) {
-    case add:
-      cnt++;
-      break;
-    case minuss:
-      cnt--;
-      break;
-    default:
-  }
-  console.log(cnt);
-  return cnt;
-};
+// store.subscribe(e => {
+//   console.log(store.getState());
+//   const todos = store.getState();
+//   ul.innerHTML = '';
+//   todos.forEach(e => {
+//     const li = document.createElement("li");
+//     const btn = document.createElement("button");
+//     btn.innerText = "DEL";
+//     btn.id = e.id;
+//     btn.addEventListener("click", e => {
+//       console.log(e.target.parentNode.id);
+//       store.dispatch({ type: "del", id: parseInt(e.target.parentNode.id) });
+//     });
+//     li.id = e.id;
+//     li.innerText = e.text;
+//     li.appendChild(btn);
+//     ul.appendChild(li);
+//   });
+// });
 
-const store = createStore(reducer);
+// const onsubmit = e => {
+//   e.preventDefault();
+//   store.dispatch({ type: "add", text: input.value });
+//   input.value = "";
+// }
+// form.addEventListener("submit", onsubmit);
 
-const onchange = e => {
-  value.innerText = store.getState();
-}
-store.subscribe(onchange);
 
-plus.addEventListener("click", e => {
-  store.dispatch({ type: add });
-});
+// function asdf() {
+//   const plus = document.getElementById("plus");
+//   const minus = document.getElementById("minus");
+//   const value = document.getElementById("value");
 
-minus.addEventListener("click", e => {
-  store.dispatch({ type: minuss });
-});
+//   const add = "add";
+//   const minuss = "minus";
+//   const reducer = (cnt = 0, action) => {
+//     switch (action.type) {
+//       case add:
+//         cnt++;
+//         break;
+//       case minuss:
+//         cnt--;
+//         break;
+//       default:
+//     }
+//     console.log(cnt);
+//     return cnt;
+//   };
+
+//   const store = createStore(reducer);
+
+//   const onchange = e => {
+//     value.innerText = store.getState();
+//   }
+//   store.subscribe(onchange);
+
+//   plus.addEventListener("click", e => {
+//     store.dispatch({ type: add });
+//   });
+
+//   minus.addEventListener("click", e => {
+//     store.dispatch({ type: minuss });
+//   });
+// }
