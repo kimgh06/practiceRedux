44d3c982e283d376338d3eae24eeb1b954c13335
kkh061101 <kkh061101@naver.com>
2023-05-10 15:19:52 +0900

    react-redux-todolist

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,25 +1,33 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import Todo from "../components/Todo";
+import { addtodo, deltodo } from "../store";
 
 function Home(props) {
-  console.log(props);
+  console.log(props.state);
   const [text, setText] = useState("");
   return <div className="home">
     <form onSubmit={e => {
       e.preventDefault();
-
+      props.addTodo(text);
+      setText("");
     }}>
       <input placeholder="put it" value={text} onChange={e => setText(e.target.value)} />
       <button>put</button>
     </form>
     <ul>
-
+      {(props.state).map((i, n) => {
+        return <Todo {...i} key={n} />
+      })}
     </ul>
   </div>;
 }
-
-const getCurrent = (state) => {
+const mapStateToProps = (state) => {
   return { state };
 }
 
-export default connect(getCurrent)(Home);
+const mapDispatchToProps = (dispatch) => {
+  return { addTodo: e => dispatch(addtodo(e)) };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
---
+++ src/components/Todo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { connect } from "react-redux";
+import { deltodo } from "../store";
+
+const Todo = ({ id, text, clicked }) => {
+  return <li id={id}>
+    {text}
+    <button onClick={e => {
+      clicked();
+    }}>del</button>
+  </li>;
+}
+
+const mapStateToProps = (dis, oprops) => {
+  return { clicked: e => dis(deltodo(oprops.id)) };
+}
+
+export default connect(null, mapStateToProps)(Todo);
