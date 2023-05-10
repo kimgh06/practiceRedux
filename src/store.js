f7d82536789b9e525c2ddf4bc13ad641288b0da9
kkh061101 <kkh061101@naver.com>
2023-05-10 21:04:22 +0900

    createReducer

--- src/store.js
+++ src/store.js
@@ -1,19 +1,26 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 export const addtodo = createAction("add");
 export const deltodo = createAction("del");
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addtodo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deltodo.type:
-      return state.filter(e => e.id !== action.payload);
-    default:
-      return state;
-  }
-}
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addtodo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deltodo.type:
+//       return state.filter(e => e.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// }
+
+const reducer = createReducer([], {
+  [addtodo]: (s, a) => {
+    s.push({ text: a.payload, id: Date.now() })
+  },
+  [deltodo]: (s, a) => s.filter(e => e.id !== a.payload),
+});
 
 const store = createStore(reducer);
 
